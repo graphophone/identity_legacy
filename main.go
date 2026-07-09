b5baadaa55ce2c80be038e6434e87cc3438c4eef
justkinou <justkinou@proton.me>
2026-07-10 00:10:00 +0200

    connect to redis database

--- main.go
+++ main.go
@@ -9,23 +9,27 @@ import (
 	"graphophone.identity/internal/database/redis"
 )
 
+const (
+	configPath = "config.local.yaml"
+)
+
 func main() {
 	ctx := context.Background()
 	fmt.Println("Hello world")
-	cfg, err := config.LoadConfig("config.local.yaml")
+	cfg, err := config.LoadConfig(configPath)
 	if err != nil {
 		log.Fatal("Error while reading config: ", err)
-		return
 	}
+
 	fmt.Printf("Redis: %s (password: %s)\n", cfg.Redis().Address, cfg.Redis().Password)
 	redisClient, err := redis.Connect(ctx, cfg.Redis())
 	if err != nil {
 		log.Fatal("Error while reading config: ", err)
-		return
 	}
+
+	fmt.Println("Connected to Redis")
 
 	if err := redisClient.Close(); err != nil {
 		log.Fatal("Error while closing redis connection: ", err)
-		return
 	}
 }
